bc9d93fe93f54ace8b7e16b153943c6d36e6b1fb
Amon-Kakutani <68629390+Amon-Kakutani@users.noreply.github.com>
2020-10-29 21:51:16 +0900

    変更

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -2,7 +2,28 @@ $(function(){
 
 let hako = 0;
 
-$("#button01").on("click",function(){for(let i=0;i<10;i++){
+$("#button01").on("click",function(){
+    for(let i=0;i<10;i++){
     hako=hako+2;
 }
 $("#content").text(hako);
+})
+
+$("#button02").on("click",function(){
+    for(let i=0;i<10;i++){
+        hako=hako+i;
+}
+$("#content").text(hako);
+})
+$("#button03").on("click",function(){
+setinterval(function(){
+    hako=hako+1;
+    $("#content").text(hako);
+},1000)
+})
+
+$("#button04").on("click",function(){
+    hako=0;
+    $("#content").text(hako);
+})
+})
